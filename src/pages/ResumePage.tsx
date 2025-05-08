784480fe43ed36bf513a10954a540b88f28f5b2a
William Whitehead <willclashtest@gmail.com>
2025-05-07 19:41:22 -0500

    update favicon and resume

--- src/pages/ResumePage.tsx
+++ src/pages/ResumePage.tsx
@@ -1,49 +1,35 @@
 import { useEffect } from "react";
-import { IconDownload } from "@tabler/icons-react"; // Import Tabler icon
-import { motion } from "framer-motion"; // Import motion for animation
-import resume from "../assets/resume.pdf";
+import { IconDownload } from "@tabler/icons-react";
 
 function ResumePage() {
   useEffect(() => {
     const embedElement = document.querySelector("embed");
     if (embedElement) {
-      embedElement.style.pointerEvents = "none"; // Disable pointer events for the embed
+      embedElement.style.pointerEvents = "none";
     }
   }, []);
 
   return (
-    <div className="max-w-4xl mx-auto p-6 bg-white ">
-      <div className="flex justify-between items-center mb-8">
-        <h1 className="text-4xl font-bold text-center mb-4 ">My Resume</h1>
+    <div className="max-w-4xl mx-auto p-6">
+      <h1 className="text-4xl font-bold text-center mb-6">My Resume</h1>
 
-        {/* Animated Download Button */}
-        <motion.button
-          whileHover={{ scale: 1.05 }} // Scale up slightly on hover
-          whileTap={{ scale: 0.95 }} // Scale down on click
-          onClick={() =>
-            window.open(
-              "https://docs.google.com/document/d/1AfyetAPTr0x9UEfcnXV7LvWgqlnbYmetNXsP6QqOhN4/export?format=pdf"
-            )
-          }
-          className="px-6 py-3 bg-blue-600 text-white rounded-lg shadow-lg flex items-center space-x-2 hover:bg-blue-700 transition duration-300"
-        >
-          <IconDownload size={24} /> {/* Tabler Download Icon */}
-          <span>Download PDF</span>
-        </motion.button>
-      </div>
-
-      <div className="relative h-[800px] w-full">
-        {/* Embed PDF */}
-        <embed
-          src={resume}
-          type="application/pdf"
-          width="100%"
-          height="150%"
-          className="border rounded-lg shadow-lg"
+      <div className="relative">
+        <img
+          src="/resume.jpg"
+          alt="Resume Preview"
+          className="w-full rounded-lg shadow-lg border"
         />
 
-        {/* Transparent overlay for custom cursor interaction */}
-        <div className="absolute inset-0 pointer-events-auto" />
+        {/* Download button in top-right corner */}
+
+        <a
+          href="/whitehead_resume.pdf"
+          download
+          className="absolute top-4 right-4 px-4 py-2 bg-blue-600 text-white rounded-md shadow-md flex items-center space-x-2 hover:bg-blue-700 transition duration-300"
+        >
+          <IconDownload size={20} />
+          <span>Download</span>
+        </a>
       </div>
     </div>
   );
