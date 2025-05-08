81be827facda22b921a0f5406f41d0ff3fe9cdc5
William Whitehead <willclashtest@gmail.com>
2025-05-07 19:26:11 -0500

    move file structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,35 @@
-import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
+import {
+  Route,
+  Routes,
+  BrowserRouter as Router,
+  useNavigate,
+} from "react-router-dom";
 import CustomCursor from "./CustomCursor";
 import "./App.css";
 import HomePage from "./pages/HomePage";
 import WiiTemplate from "./pages/WiiTemplate";
 import HealthWarningScreen from "./components/StartScreen";
+import { useEffect } from "react";
 
 function App() {
+  function RedirectHandler() {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+      const redirectPath = sessionStorage.redirect;
+      if (redirectPath) {
+        sessionStorage.removeItem("redirect");
+        navigate(redirectPath, { replace: true });
+      }
+    }, [navigate]);
+
+    return null;
+  }
   return (
     <div className="gridlines">
       <CustomCursor />
       <Router>
+        <RedirectHandler />
         <Routes>
           <Route path="/" element={<HealthWarningScreen />} />
           <Route path="/home" element={<HomePage />} />
